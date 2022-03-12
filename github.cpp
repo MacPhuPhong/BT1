3ff267adcd33298f46e7fef1eddffe8e1750d3f9
MacPhuPhong <101451390+MacPhuPhong@users.noreply.github.com>
2022-03-12 15:41:30 +0700

    Add files via upload

---
+++ github.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	cout<<"MacPhuPhong"<<endl<<"106210059"<<endl<<"21DTCLC1";
+	return 0;
+}
